99fc426119958fc0b490c1c98577daaa7006d8d5
Todd Lang <todd.lang@d2l.com>
2025-07-21 10:47:11 -0400

    Testing update to location of sqlcmd

--- db.sh
+++ db.sh
@@ -1,10 +1,12 @@
 set -eu
 
 invokesql() {
-  /opt/mssql-tools/bin/sqlcmd \
+  /opt/mssql-tools18/bin/sqlcmd \
   -S 127.0.0.1,1433 \
   -U SA \
   -P $MSSQL_SA_PASSWORD \
+  -C \
+  -Q \
   "$@"
 }
 
